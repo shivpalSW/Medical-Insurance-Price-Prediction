0e05f98a7582ef7fff9b5beb549552c2c9905360
shivpalSW <shivpal.wadkar2050@gmail.com>
2022-12-11 00:28:49 +0530

    linear Model

---
+++ linear_model.py
@@ -0,0 +1,13 @@
+# %%time
+import pandas as pd
+import numpy as np 
+
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+
+from sklearn.metrics import confusion_matrix,plot_confusion_matrix,classification_report
+
+from sklearn.metrics import precision_score,recall_score,f1_score,accuracy_score,roc_auc_score,roc_curve
+
+import matplotlib.pyplot as plt 
+import seaborn as sns
